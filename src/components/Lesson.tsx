b53781f0ecb30550f159213fad5a9578a546402d
Tiago Damasceno <tiagodepd@yahoo.com.br>
2022-07-19 16:13:27 -0300

    add ;

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,6 @@
-import { CheckCircle, Lock } from 'phosphor-react'
-import {isPast, format } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { CheckCircle, Lock } from 'phosphor-react';
+import {isPast, format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { Link, useParams } from 'react-router-dom';
 import classNames from 'classnames';
 
